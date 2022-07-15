4cdce781f0ddd43a68f81dd5ea4e48da4cde6f5e
Mithul <padmamithul123@gmail.com>
2022-07-15 22:26:25 +0530

    first commit

---
+++ Math.py
@@ -0,0 +1,9 @@
+print("I will count my chickens")
+
+print("Hens",25+30/6)
+print("Roosters",100-25*3/4)
+#+ ->addition
+#- -> subtraction
+#% ->modulus
+#/ ->division
+#* ->product
---
+++ printing.py
@@ -0,0 +1,6 @@
+print("Hello worl")
+print("Why??")
+print("i am human")
+print("This is fun")
+#print("Yay! printing") #commenting
+print("sup loosers")
