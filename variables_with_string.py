6b9b84c334af5772069eabd5db4405f3d4038270
Mithul <padmamithul123@gmail.com>
2022-07-16 07:18:29 +0530

    some variables with strings

---
+++ variables_with_string.py
@@ -0,0 +1,18 @@
+my_name = 'mithul'
+my_age = 17
+my_height = 170 #cm
+my_weight = 60 #kg
+my_eyes = 'black'
+my_hair = 'black'
+my_teeth = 'white'
+
+print(f"Lets talk about {my_name}")
+print(f"He's {my_height} cms tall.")
+print(f"He is {my_weight} kg heavy")
+print("Actually that's not too heavy.")
+print(f"He's got {my_eyes} eyes and {my_hair} hair.")
+print(f"His teeth are usually {my_teeth} depending on the coffee.")
+
+total = my_age + my_height + my_weight
+
+print(f"If i add {my_age}, {my_height}, and {my_weight} we get {total}")
